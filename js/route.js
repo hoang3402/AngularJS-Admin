8fdf066e4327898d14ce2550fb2f9d4900b00641
HoangDSR <nvh.02021995@gmail.com>
2023-06-17 22:31:11 +0700

    Init, fix, install npm

---
+++ js/route.js
@@ -0,0 +1,17 @@
+export function config($routeProvider) {
+    $routeProvider.when('/table/:name/:id', {
+        template: '<edittable></edittable>',
+    });
+    $routeProvider.when('/table/:name', {
+        template: '<datatable></datatable>',
+    });
+    $routeProvider.when('/create/:name', {
+        template: '<create></create>',
+    });
+    $routeProvider.when('/create-movie', {
+        template: '<create-movie></create-movie>',
+    });
+    $routeProvider.otherwise({
+        template: '<h1 class="text-center">Hello Admin</h1>',
+    });
+}
